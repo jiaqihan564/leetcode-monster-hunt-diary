e78c81f4c35282c0f39b105bab9eb9ca44cea374
JiaQiHan <qihan.jia@icesimba.cn>
2025-12-01 17:08:31 +0800

    feat(88): 实现合并两个有序数组的功能
    - 添加 merge1 函数用于合并两个有序数组
    - 实现从后往前的双指针比较逻辑
    - 确保在访问数组元素前检查索引边界
    - 在主函数中测试 merge1 的功能

--- 88/main.go
+++ 88/main.go
@@ -35,6 +35,25 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 }
 
+func merge1(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+
+	for j >= 0 {
+		// 一定要先判断 i >= 0，再去访问 nums1[i]
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+
+}
+
 func main() {
 	nums1 := []int{1, 2, 3, 0, 0, 0}
 	nums2 := []int{2, 5, 6}
